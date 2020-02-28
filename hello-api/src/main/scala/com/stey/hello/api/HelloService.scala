161089c5fdd9629bf785b383fa31dd95728a131d
Julien Choulet <jchoulet@blue-reef-ltd.com>
2020-02-28 12:02:39 +0800

    New service with UTF-8

--- hello-api/src/main/scala/com/stey/hello/api/HelloService.scala
+++ hello-api/src/main/scala/com/stey/hello/api/HelloService.scala
@@ -2,17 +2,21 @@ package com.stey.hello.api
 
 import akka.NotUsed
 import com.lightbend.lagom.scaladsl.api.{Descriptor, Service, ServiceCall}
+import com.lightbend.lagom.scaladsl.api.deser.MessageSerializer
 
 trait HelloService extends Service {
 
   def hello(greet: String): ServiceCall[NotUsed, String]
+  def nihao(greet: String): ServiceCall[NotUsed, String]
 
   override def descriptor: Descriptor = {
     import Service._
 
     named("stey-hello")
       .withCalls(
-        pathCall("/hello/:greet", hello _)
+        pathCall("/hello/:greet", hello _),
+        pathCall("/nihao/:greet", nihao _)
+        (MessageSerializer.NotUsedMessageSerializer, new TextMessageSerializer)
       )
       .withAutoAcl(true)
   }
